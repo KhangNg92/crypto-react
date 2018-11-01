6d9a72c4f94cefffe9a7b6c1924b86d5c6a3ac3b
Khang Nguyen <mkavo92@gmail.com>
2018-11-01 02:35:15 -0500

    adding components to app.js

--- src/App.js
+++ src/App.js
@@ -3,25 +3,36 @@ import React, { Component } from 'react';
 import './App.css';
 import styled from 'styled-components'
 
-const CustomElement = styled.div`
-font-size: 30px;
+const Logo = styled.div`
+font-size: 1.5em;
 `
-const BlueElement = styled.div`
-color: blue;
+const Button = styled.div`
 `
-
+// if we use 1fr it will divide the spacing of each components equally
+// if we use auto 100px 100px it will push those 2 buttons to the right 
+// and the logo will stay to the left
+const Wrapper = styled.div`
+display: grid;
+grid-template-columns:  auto 100px 100px;
+padding: 40px
+`
 
 class App extends Component {
   render() {
     return (
-        <div>
-          <CustomElement>
-        Hello
-        </CustomElement>
-        <BlueElement>
-          Hello Blue
-          </BlueElement>
-          </div>
+      <Wrapper>
+        <Logo>
+        CryptoDash
+        </Logo>
+        {/* <div>
+          </div> */}
+        <Button>
+        Dashboard
+          </Button>
+        <Button>
+        Settings
+          </Button>
+          </Wrapper>
     );
   }
 }
